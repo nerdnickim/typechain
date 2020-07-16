ace030336aaea7cc521f48b71b6ec25a8f7d6656
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-07-16 15:49:09 +0900

    block chain #2

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,44 @@
+import * as CryptoJs from "crypto-js";
+
 class Block {
 	public index: number;
 	public hash: string;
-	public previoushash: string;
+	public previousHash: string;
 	public data: string;
 	public timestamp: number;
+
+	static calculatorBlockHash = (
+		index: number,
+		previousHash: string,
+		timestamp: number,
+		data: string
+	): string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+
 	constructor(
 		index: number,
 		hash: string,
-		previoushash: string,
+		previousHash: string,
 		data: string,
 		timestamp: number
 	) {
 		this.index = index;
 		this.hash = hash;
-		this.previoushash = previoushash;
+		this.previousHash = previousHash;
 		this.data = data;
 		this.timestamp = timestamp;
 	}
 }
 
+Block.calculatorBlockHash;
+
 const genesisBlock: Block = new Block(0, "20200202", "", "Hello", 123456);
 
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
 
-console.log(blockChain);
+const getBlockchain = (): Block[] => blockChain;
+
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
